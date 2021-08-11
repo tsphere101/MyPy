4171e77da1704c122b480e5e10a71539994dde3e
Krissada Sarawit <topfeekrissada@outlook.com>
2021-08-12 04:29:11 +0700

    updated pyramid

--- datastruct/c1_4_pyramid.py
+++ datastruct/c1_4_pyramid.py
@@ -1,34 +1,17 @@
 
-def pyramid(n):
-    _top = 4*(n-1)+1
-    for row in range(_top):  # ROW
-        for col in range(_top):  # COL
-            if row == 0 or row == _top-1:
-                print("#", end="")
-            elif row % 2 == 0 and col >= row and row+col < _top:
-                print("#", end="")
-            elif row+col > _top-2 and col <= row and row > (_top // 2)+1 and row % 2 == 0:
-                print("#", end="")
-            elif col % 2 == 0 and col <= row and row + col <= _top-1:
-                print("#", end="")
-            elif col % 2 == 0 and col >= row and row+col > _top-1:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-    return
-
-
-if __name__ == "__main__":
-    print("*** Fun with Drawing ***")
-    valid = False
-    while not valid:
-        try:
-            n = int(input("Enter input : "))
-            if n < 2:
-                raise ValueError
-            valid = True
-        except Exception as e:
-            pass
-
-    pyramid(n)
+print("*** Fun with Drawing ***")
+n = int(input("Enter input : "))
+width = 4*(n-1)+1
+for i in range(width):
+    for j in range(width):
+        if i == 0 or i == width-1:
+            print("#", end="")
+        elif i+j <= width-1 and j % 2 == 0 and j <= i or j % 2 == 0 and j >= i and i+j > width-1:
+            print("#", end="")
+        elif i+j < width and i % 2 == 0 and j >= i :
+            print("#", end="")
+        elif i+j > width-2 and j <= i and i > (int(width/2))+1 and i % 2 == 0:
+            print("#", end="")
+        else:
+            print(".", end="")
+    print("")
