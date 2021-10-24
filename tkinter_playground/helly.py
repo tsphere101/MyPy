079fdd0a9a6c76226c52a99e08d71dddeeef8c2d
Krissada Sarawit <topfeekrissada@outlook.com>
2021-10-24 13:30:42 +0700

    Say hello to the tkinter

---
+++ tkinter_playground/helly.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+label1= Label(root,text = "Hello World!")
+label2= Label(root,text = "My name is Topfee Krissada!")
+
+label1.grid(row=0,column=0)
+label2.grid(row=1,column=0)
+
+
+
+
+
+root.mainloop()
