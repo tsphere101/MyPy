a41e3ea1f1f2477b22b62b24a0108575f413d914
Krissada Sarawit <topfeekrissada@outlook.com>
2021-10-30 01:42:20 +0700

    Updated AVLTree.py

--- datastruct/MyModule/AVLTree.py
+++ datastruct/MyModule/AVLTree.py
@@ -1,136 +1,252 @@
 
-class NodeAVL:
-    def __init__(self, data):
-        self.data = [data]
+class Node:
+    def __init__(self, data=None):
+        self.data = data
         self.left = None
         self.right = None
+        self.height = 0
 
-    def add_child(self, data):
-        if data == self.data[0]:
-            self.data.append(data)
-        elif data < self.data[0]:
-            if self.left:
-                self.left.add_child(data)
+    def __repr__(self):
+        return str(self.data)
+        # return "[node:" + str(self.data) + ']'
+
+
+class AVL:
+    def __init__(self):
+        self.root = None
+        self.length = 0
+
+    def insert(self, data):
+        if not self.root:
+            self.root = Node(data)
+            self.length += 1
+        else:
+            self.root = self._insert(data, self.root)
+
+    def _insert(self, data, node):
+        if data < node.data:
+            if node.left:
+                node.left = self._insert(data, node.left)
+                return self._rebalance(node)
             else:
-                self.left = NodeAVL(data)
-        elif data > self.data[0]:
-            if self.right:
-                self.right.add_child(data)
+                node.left = Node(data)
+                self.length += 1
+                return node
+        elif data > node.data:
+            if node.right:
+                node.right = self._insert(data, node.right)
+                return self._rebalance(node)
             else:
-                self.right = NodeAVL(data)
+                node.right = Node(data)
+                self.length += 1
+                return node
 
-        balance_factor = self.get_balance_factor()
-        if balance_factor > 1 and data < self.left.data[0]:
-            # left is heavier than right, perfor right rotation
-            return self.rotate_right()
-        
-        if balance_factor > 1 and data > self.left.data[0]:
-            # left is heavier than right
-            self.left.rotate_left()
-            return self.rotate.right()
+    def _rebalance(self, node):
+        bf = self.balance_factor(node)
+        if bf <= -2:
+            # Right heavy
+            return self.rotate_left(node)
 
-        if balance_factor < -1 and data > self.right.data[0]:
-            # right is heavier than left
-            return self.rotate_left()
-        if balance_factor < -1 and data < self.right.data[0]:
-            self.right.rotate_right()
-            return self.rotate_left()
+        elif bf >= 2:
+            # Left heavy
+            return self.rotate_right(node)
 
-    def rotate_right(self,z):
-        y = z.left
-        T3 = y.right
- 
-        # Perform rotation
-        y.right = z
-        z.left = T3
+        else:
+            return node
 
-    def rotate_left(self,z):
-        y = z.right
+    def rotate_left(self, x):
+        # prep
+        y = x.right
         T2 = y.left
- 
-        # Perform rotation
-        y.left = z
-        z.right = T2
 
-    def get_balance_factor(self):
-        if self.left:
-            left_height = self.left.get_height()
+        # perform
+        x.right = T2
+        y.left = x
+        return y
+
+    def rotate_right(self, x):
+        # prep
+        y = x.left
+        T2 = y.right
+
+        # perform
+        x.left = T2
+        y.right = x
+        return y
+
+    def insert_no_return(self, data):
+        if not self.root:
+            self.root = Node(data)
+            self.length += 1
         else:
-            left_height = 0
-        if self.right:
-            right_height = self.right.get_height()
-        else:
-            right_height = 0
+            self._insert_no_return(data, self.root)
+            self.root = self._rebalance(self.root)
 
-        return left_height - right_height
+    def _insert_no_return(self, data, node):
+        if data < node.data:
+            if node.left:
+                self._insert_no_return(data, node.left)
+                node.left = self._rebalance(node.left)
+            else:
+                node.left = Node(data)
+                self.length += 1
 
-    def get_height(self):
-        if self.left:
-            left_height = self.left.get_height()
-        else:
-            left_height = 0
+        elif data > node.data:
+            if node.right:
+                self._insert_no_return(data, node.right)
+                node.right = self._rebalance(node.right)
+            else:
+                node.right = Node(data)
+                self.length += 1
 
-        if self.right:
-            right_height = self.right.get_height()
-        else:
-            right_height = 0
-
-        child_height = left_height if left_height > right_height else right_height
-        return child_height + 1
-
-    def in_order_traversal(self):
-
+    def in_order_traversal(self, node=None):
         elements = []
+        if self.root is None:
+            return None
+        if node is None:
+            node = self.root
 
-        if self.left:
-            elements += self.left.in_order_traversal()
+        if node.left:
+            elements += self.in_order_traversal(node.left)
 
-        elements += self.data
+        elements.append(node.data)
 
-        if self.right:
-            elements += self.right.in_order_traversal()
+        if node.right:
+            elements += self.in_order_traversal(node.right)
 
         return elements
 
-    def printHelper(self, root, indent, last):
-        if root != None:
-            print(indent, end='')
-            if last:
-                print("R----", end='')
-                indent += "     "
-            else:
-                print("L----", end='')
-                indent += "|    "
-            print(root.data[0])
-            self.printHelper(root.left, indent, False)
-            self.printHelper(root.right, indent, True)
+    def _in_order_successor(self, node):
+        if node.right:
+            # Case I
+            succ = node.right
+            while succ.left:
+                succ = succ.left
+            return succ
 
+        if not node.right:
+            # Case II
+            p = self.root
+            while (p is not None):
+                if node.data < p.data:
+                    # p is greather than the node; might be the successor
+                    succ = p
+                    p = p.left
+                if node.data > p.data:
+                    # p is less than the node; should go right for being greather
+                    p = p.right
 
-class AVLTree:
-    def __init__(self):
-        self.root = None
+            return succ
 
-    def append(self, data):
+    def delete(self, data):
         if self.root is None:
-            self.root = NodeAVL(data)
-        else:
-            self.root.add_child(data)
+            raise ValueError
 
-    def __str__(self):
-        elements = self.root.in_order_traversal()
+        self.root = self._delete(data, self.root)
+        self.length -= 1
+        self.root = self._rebalance(self.root)
 
-        return "[" + ", ".join(str(x) for x in elements)+"]"
+    def _delete(self, data, node):
 
-    # Print the tree
-    def draw_tree(self):
-        self.root.printHelper(self.root, "", True)
+        if node is None:
+            raise ValueError(f"{data} is not contained in the tree")
 
-from betsybaileyy_AVLTree import AVLTree as Avl
+        if data == node.data:
+            # 4 State:
+            if not node.left and not node.right:
+                # Has no child
+                return None
+
+            if node.left and not node.right:
+                # Has only left child
+                return node.left
+
+            if not node.left and node.right:
+                # Has only right child
+                return node.right
+
+            if node.left and node.right:
+                # Has both child
+
+                # replace the deleted node with the successor
+                succ = self._in_order_successor(node)
+                node.data = succ.data
+
+                # delete old successor node
+                node.right = self._delete(succ.data, node.right)
+
+                return node
+
+        elif data < node.data:
+            node.left = self._delete(data, node.left)
+            return self._rebalance(node)
+
+        elif data > node.data:
+            node.right = self._delete(data, node.right)
+            return self._rebalance(node)
+
+        raise ValueError
+
+    def is_empty(self):
+        return self.root is None
+
+    def is_not_empty(self):
+        return self.root is not None
+
+    def height(self):
+        return self._height(self.root)
+
+    def _height(self, node=None):
+        if self.root is None:
+            return None
+
+        if node is None:
+            return 0
+
+        return 1 + max(self._height(node.left), self._height(node.right))
+
+    def balance_factor(self, node):
+        if node is None:
+            node = self.root
+
+        try:
+            hl = self._height(node.left)
+        except:
+            hl = 0
+
+        try:
+            hr = self._height(node.right)
+        except:
+            hr = 0
+
+        return hl-hr
+
+    def __len__(self):
+        return self.length
+
+    def _printTree(self, node, level=0):
+        if node != None:
+            self._printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self._printTree(node.left, level + 1)
+
+    def print_tree(self):
+        self._printTree(self.root)
 
 if __name__ == "__main__":
-    a = Avl()
+    t = AVL()
+    t.insert(16)
+    t.insert(8)
+    t.insert(25)
+    t.insert(18)
+    t.insert(52)
 
-    for i in range(50):
-        a.insert(i)
-    a.items_in_order()
+    # for i in range(31):
+    #     t.insert(i)
 
+    t.print_tree()
+
+    print(t.in_order_traversal())
+    print("tree height",t.height())
+    print("Balance factor of root",t.balance_factor(t.root))
+    print("len",len(t))
