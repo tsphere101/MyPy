6bce167787f2889309526f44381f207fd3f0ca4e
Krissada Sarawit <topfeekrissada@outlook.com>
2021-11-01 01:22:27 +0700

    backup

--- linear project/test_searchengine.py
+++ linear project/test_searchengine.py
@@ -2,16 +2,14 @@ from SearchEngine import *
 import time
 if __name__ == "__main__":
     se = SearchEngine("zen_record.txt")
-    sp = SpellCorrection(dictionary_path="dictionary.txt")
 
-    kw = input()
-    t0 = time.time()
-    x = se.search_by_keyword(keyword=kw, return_value ='Quote',number_of_result = 20)
-    t1 = time.time()
-    if x:
+    while True:
+        kw = input("Search : ")
+        t0 = time.time()
+        x = se.search_by_keyword(keyword=kw, return_value ='Quote',number_of_result = 20)
+        t1 = time.time()
+
         for i,data in enumerate(x):
             print(i+1,data.replace(kw,"'"+kw+"'"))
-    else :
-        print('DOES NOT MATH ANY WORDS.')
-    
-    print(t1-t0)
+        
+        print("Searched in",t1-t0,"seconds")
